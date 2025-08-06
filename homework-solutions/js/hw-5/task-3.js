b4b294f565ea0adb1bed7a65b26b93e4f299580a
Vadzim Khomich <vadzim.khomich@activeplatform.com>
2025-08-06 14:36:44 +0300

    add solutions HW5

--- homework-solutions/js/hw-5/task-3.js
+++ homework-solutions/js/hw-5/task-3.js
@@ -6,5 +6,20 @@
 const word = 'hello';
 
 let vowelsAndConsonantsResult = '';
+let vowels = 'aeiou';
+let vowelsInWord = 0;
+let consonantsInWord = 0;
+
+for (let i = 0; i < word.length; i++) {
+  const regex = /^[A-Za-z]+$/;
+  const letter = word[i].toLocaleLowerCase()
+  if (regex.test(letter) && vowels.includes(letter)) {
+    vowelsInWord++;
+  } else if(regex.test(letter) && !vowels.includes(letter)) {
+    consonantsInWord++;
+  }
+}
+
+vowelsAndConsonantsResult = `${word} contains ${vowelsInWord} vowels and ${consonantsInWord} consonants`
 
 export { vowelsAndConsonantsResult };
