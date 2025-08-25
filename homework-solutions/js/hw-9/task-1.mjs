206c02851757b6f8f1ce1eca680b9ee20cbf1760
Vadzim Khomich <vadzim.khomich@activeplatform.com>
2025-08-25 16:44:40 +0300

    HW9

--- homework-solutions/js/hw-9/task-1.mjs
+++ homework-solutions/js/hw-9/task-1.mjs
@@ -11,10 +11,10 @@
 const character = { name: 'Barney', age: 36, gender: 'male', isQa: true };
 
 // 1
-let keyWithFourChars;
+const keyWithFourChars = Object.keys(character).filter(key => key.length === 4)
 
 // 2
-let stringValues;
+const stringValues = Object.values(character).filter(value => typeof value === "string")
 
 // 3
 /* Do not touch this part */
@@ -22,10 +22,14 @@ const logSpy = jest.spyOn(console, 'log');
 // ---
 
 // Ваш код
-let keyValuePairs;
+const keyValuePairs = Object.entries(character)
+
+for(const el of keyValuePairs) {
+  console.log(`key = ${el[0]}, value = ${el[1]}`)
+}
 
 // 4
-let hasSalaryKey1stOption;
-let hasSalaryKey2ndOption;
+const hasSalaryKey1stOption = "salary" in character
+const hasSalaryKey2ndOption = character.hasOwnProperty("salary")
 
 export { keyWithFourChars, stringValues, hasSalaryKey1stOption, hasSalaryKey2ndOption, keyValuePairs, logSpy };
